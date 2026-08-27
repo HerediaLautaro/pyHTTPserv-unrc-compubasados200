1cbbc8967a53c8a28a93299293514763341db6b4
Lautaro <heredialauta99@gmail.com>
2026-08-26 21:42:09 -0300

    se arreglaron bugs en los verbos DELETE, GET y PATCH

--- httpServ.py
+++ httpServ.py
@@ -23,7 +23,6 @@ def thatServer(environ, start_response):
 
     if request == "GET":
         if len(path) == 3:
-            start_response(status, headers)
             key = path[2]
             key = int(key)
             flag = 0
@@ -34,20 +33,26 @@ def thatServer(environ, start_response):
                     break
             
             if flag == 1:
+                status = "200 OK"
+                start_response(status, headers)
                 return [response.encode('utf-8'), b"\n"]
             else:
                 status = "404 Not Found"
                 start_response(status, headers)
-                return [b"tarea inexistente"]
-        else:
+                return [b"Not Found"]
+        elif len(path) == 2:
             start_response(status, headers)
             response = json.dumps(LIST)
             return  [response.encode('utf-8'), b"\n"]#ya funciona
+        else:
+            status = "404 Not Found"
+            start_response(status, headers)
+            return[b"Not Found"]
         
     elif request == "POST" :
         x = int(environ.get('CONTENT_LENGTH', 0))
         body = environ['wsgi.input'].read(x)
-        
+
         tarea = json.loads(body)
 
         tarea["id"] = ID
@@ -61,21 +66,53 @@ def thatServer(environ, start_response):
         return [response.encode('utf-8'), b"\n"]
 
     elif request == "PATCH":
-        start_response(status, headers)
-        return [b"no implementado"]
-    elif request == "DELETE":
-        if len(path) == 3 and len(LIST) >= 1:
-            start_response(status, headers)
-            index = path[2]
-            index = int(index)
-            for i in range (0, len(LIST)):
-                if(LIST[i][id] == index):
-                    del LIST[index]
-                    return [b"la tarea fue eliminada con exito"]
+        if len(path) == 3 :
+            x = int(environ.get('CONTENT_LENGTH', 0))
+            task = environ['wsgi.input'].read(x)
+
+            task = json.loads(task)
+            flag = 0
+            for i in range(0, len(LIST)):
+                if LIST[i]["id"] == int(path[2]):
+                    LIST[i]["done"] = task["done"]
+                    flag = 1
+                    response = json.dumps(LIST[i])
+                    break
+            
+            if flag == 1:
+                status = "200 OK"
+                start_response(status, headers)
+                return [response.encode('utf-8'),b"\n"]
+            else:
+                status = "404 Not Found"
+                start_response(status, headers)
+                return [b"Not Found"]
         else:
             status = "404 Not Found"
             start_response(status, headers)
-            return [b"no existe tal elemento"]
+            return [b"Not Found"]
+        
+    elif request == "DELETE":
+        if len(path) == 3 and len(LIST) >= 1:
+            index = path[2]
+            index = int(index)
+            flag = 0
+            for i in range (0, len(LIST)):
+                if(LIST[i]["id"] == index):
+                    del LIST[i]
+                    flag = 1
+                    break
+            if flag == 1:
+                start_response(status, headers)
+                return [b"OK"]
+            else:
+                status = "404 Not Found"
+                start_response(status, headers)
+                return [b"Not Found"]
+        else:
+            status = "204 No Content"
+            start_response(status, headers)
+            return [b"No Content"]
 
         
 
